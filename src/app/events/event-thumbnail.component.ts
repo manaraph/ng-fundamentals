5a8a774caecf16ed79e2663c558de9a64c09e6ea
Manasseh Omachonu <manaraph225@gmail.com>
2019-08-12 00:17:15 +0100

    *ngIf and hidden attribute

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,15 +8,16 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
       <div>Date: {{program?.date}}</div>
       <div>Time: {{program?.time}}</div>
       <div>Price: \${{program?.price}}</div>
-      <div>
+      <div *ngIf="program?.location">
         <span>Location: {{program?.location?.address}}</span>
         <span class="pad-left">{{program?.location?.city}}, {{program?.location?.country}}</span>
       </div>
-      <div>Online Url: {{ program?.onlineUrl}} </div>
-      <button class="btn btn-primary">Click me!</button>
+      <div [hidden]="!program?.onlineUrl">Online Url: {{ program?.onlineUrl}} </div>
+      <button id="myButton" class="btn btn-primary">Click me!</button>
     </div>
   `,
   styles: [`
+    #myButton { margin: 10px auto; }
     .thumbnail { min-height: 250px }
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
