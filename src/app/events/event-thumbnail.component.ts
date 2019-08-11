4f09c102e9c7952547ba5535f5593541ff757bc3
Manasseh Omachonu <manaraph225@gmail.com>
2019-08-11 23:33:47 +0100

    ngFor

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -12,28 +12,16 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
         <span>Location: {{program.location.address}}</span>
         <span class="pad-left">{{program.location.city}}, {{program.location.country}}</span>
       </div>
-      <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>
+      <button class="btn btn-primary">Click me!</button>
     </div>
   `,
   styles: [`
+    .thumbnail { min-height: 250px }
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
   `]
 })
 
 export class EventThumbnailComponent {
-  @Input() program: any
-  someProperty: any = "Value from child component"
-  @Output() eventClick = new EventEmitter();
-
-  handleClickMe() {
-    // console.log('Clicked');
-    this.eventClick.emit(this.program.name);
-    
-  }
-
-  logFoo(){
-    console.log('Foo');
-    
-  }
+  @Input() program: any 
 }
