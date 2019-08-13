4aa0fe83e93e9f441da63bbdae77753668b5ff08
Manasseh Omachonu <manaraph225@gmail.com>
2019-08-13 02:35:44 +0100

    Orgainizing files with barrels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 // import { AppRoutingModule } from './app-routing.module';
+import { 
+  EventListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver,
+} from "./events/index";
 import { EventsAppComponent } from './events-app.component';
-import { EventListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.service';
 
 @NgModule({
   declarations: [
