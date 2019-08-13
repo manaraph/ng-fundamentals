ea56505797300a9df80e48164459ac2359b342ef
Manasseh Omachonu <manaraph225@gmail.com>
2019-08-13 15:52:37 +0100

    Validating reactive forms

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ProfileComponent } from './profile.component';
 import { userRoutes } from './user.routes';
 import { LoginComponent } from './login.component';
@@ -10,6 +10,7 @@ import { LoginComponent } from './login.component';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(userRoutes)
   ],
   declarations: [
