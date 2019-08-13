578232954ee9ad7ec48b446735ddb0a9a3805819
Manasseh Omachonu <manaraph225@gmail.com>
2019-08-13 02:18:57 +0100

    Loading feature module

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -12,5 +12,6 @@ export const appRoutes:Routes  = [
   // { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
   { path: 'events/:id', component: EventDetailsComponent },
   { path: '404', component: Error404Component },
-  { path: '', redirectTo: '/events', pathMatch: 'full' }
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'user', loadChildren: './user/user.module#UserModule' }
 ]
