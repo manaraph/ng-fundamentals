52856ff97e500832628786124cbf22a029545cf0
Manasseh Omachonu <manaraph225@gmail.com>
2019-08-14 23:54:59 +0100

    shared custom validation

---
+++ src/app/events/shared/restricted-words.validator.ts
@@ -0,0 +1,11 @@
+import { FormControl } from '@angular/forms';
+
+  export function restrictedWords(words) { 
+    return (control: FormControl): {[key: string]: any} => {
+      if(!words) return null
+      let invalidWords = words
+        .map( word  => control.value.includes(word) ? word : null )
+        .filter( word => word != null);
+      return invalidWords && invalidWords.length > 0 ? {'restrictedWords': invalidWords.join(', ') } : null;
+    }
+  }
