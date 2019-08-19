e4e6fda19d121d6b4f7064edeeccba57e4e535b7
Manasseh Omachonu <manaraph225@gmail.com>
2019-08-19 01:08:41 +0100

    Fix username syntac erroe

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
   constructor(private authService:AuthService, private router:Router){}
-  username
+  userName
   password
   mouseoverLogin
   login(formValues){
