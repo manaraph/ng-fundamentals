fca2db73008c6d9277b2b2dbd61a19b3ea23a8ea
Manasseh Omachonu <manaraph225@gmail.com>
2019-08-21 02:17:50 +0100

    Multiple content projection

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -4,13 +4,14 @@ import { Component, Input } from '@angular/core';
   selector: 'collapsible-well',
   template: `
     <div (click)="toggleContent()" class="well pointable">
-      <h4 class="well-title">{{title}}</h4>
-      <ng-content *ngIf="visible"></ng-content>
+      <h4 class="well-title">
+        <ng-content select="[well-title]"></ng-content>
+      </h4>
+      <ng-content *ngIf="visible" select="[well-body]"></ng-content>
     </div>
   `
 })
 export class CollapsibleWellComponent {
-  @Input() title: string;
   visible: boolean = false;
 
   toggleContent() {
