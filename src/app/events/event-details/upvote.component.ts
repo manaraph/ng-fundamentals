45bd032a8b84867dbe4d8111c01453b1746db924
Manasseh Omachonu <manaraph225@gmail.com>
2019-08-26 17:59:27 +0100

    input setters

--- src/app/events/event-details/upvote.component.ts
+++ src/app/events/event-details/upvote.component.ts
@@ -6,8 +6,7 @@ import { Component, Input, EventEmitter, Output } from '@angular/core';
     <div class="votingWidgetContainer pointable" (click)="onClick()">
       <div class="well votingWidget">
         <div class="votingButton">
-          <i *ngIf="voted" class="glyphicon glyphicon-heart"></i>
-          <i *ngIf="!voted" class="glyphicon glyphicon-heart-empty"></i>
+          <i class="glyphicon glyphicon-heart" [style.color]="iconColor"></i>
         </div>
         <div class="badge badge-inverse votingCount"><div>{{count}}</div></div>
       </div>
@@ -18,8 +17,11 @@ import { Component, Input, EventEmitter, Output } from '@angular/core';
  
 export class UpvoteComponent {
   @Input() count: number;
-  @Input() voted: boolean;
+  @Input() set voted(val) {
+    this.iconColor = val ? 'red' : 'white';
+  };
   @Output() vote = new EventEmitter();
+  iconColor: string;
 
   onClick() {
     this.vote.emit({});
