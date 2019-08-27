252d688952e02330785232a588d667b234f6de0c
Manasseh Omachonu <manaraph225@gmail.com>
2019-08-27 18:14:25 +0100

    Integrating and persisting authentication on the server

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,20 +1,40 @@
 import { Injectable } from "@angular/core";
 import { IUser } from './user.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable()
 export class AuthService {
   currentUser: IUser;
-  loginUser(userName:string, password:string){
-    this.currentUser = {
-      id: 1,
-      userName: userName,
-      firstName: 'John',
-      lastName: 'Smith'
-    }
+  
+  constructor(private http: HttpClient) {}
+
+  loginUser(userName:string, password:string) {
+    const loginInfo = { username: userName, password: password };
+    const options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' })}
+    
+    return this.http.post('/api/login', loginInfo, options)
+      .pipe(tap(data => {
+        this.currentUser = <IUser>data['user'];
+      }))
+      .pipe(catchError(err => {
+        return of(false)
+      }))
   }
 
   isAuthenticated() {
     return !!this.currentUser;
+  }
+
+  checkAuthenticationstatus() {
+    this.http.get('/api/currentIdentity')
+      .pipe(tap(data => {
+        if(data instanceof Object) {
+          this.currentUser = <IUser>data;
+        }
+      }))
+      .subscribe()
   }
 
   updateCurrentUser(firstName:string, lastName:string){
--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -9,13 +9,22 @@ import { Router } from '@angular/router';
   `]
 })
 export class LoginComponent {
-  constructor(private authService:AuthService, private router:Router){}
   userName
   password
   mouseoverLogin
+  loginInvalid = false;
+
+  constructor(private authService:AuthService, private router:Router){}
+  
   login(formValues){
-    this.authService.loginUser(formValues.userName, formValues.password);
-    this.router.navigate(['events']);
+    this.authService.loginUser(formValues.userName, formValues.password)
+      .subscribe(resp => {
+        if(!resp) {
+          this.loginInvalid = true;
+        } else {
+          this.router.navigate(['events']);
+        }
+      })
   }
 
   cancel(){
