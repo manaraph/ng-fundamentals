f4517ef57319f43cd700a5ff1d726dee8859a155
Manasseh Omachonu <manaraph225@gmail.com>
2019-08-29 22:12:31 +0100

    Fixes build house-keeping

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -4,4 +4,4 @@ import { LoginComponent } from './login.component';
 export const userRoutes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'login', component: LoginComponent }
-]
+];
